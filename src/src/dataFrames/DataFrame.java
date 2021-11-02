be96d0c3b4b2a8f864f006d41f0bc6d2d7894810
Ismael Molinos <ismael.molinos@estudiants.urv.cat>
2021-11-02 15:28:29 +0100

    Comentarios de dudas y ficheros de ejemplo

--- src/src/dataFrames/DataFrame.java
+++ src/src/dataFrames/DataFrame.java
@@ -5,13 +5,13 @@ import java.util.Map;
 
 public interface DataFrame extends Iterable<Integer> {
 
-    //Matriz? filas i columnas...
+    //Matriz? filas i columnas -> Map de Listas, cada lista es una columna
 
     int at(int row, String column);
     int iat(int row, int column);
     int columns();
     int size();
     int[] sort(String column, Comparator<Integer> integerComparator);
-    //DataFrame query(Condicion???);
+    //DataFrame query(Condicion???) -> Predicate;
 
 }
